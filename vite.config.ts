fbaea292be270ff17e0a2116f0dc3bd6832ef2c9
isak-martinson-sl <isak@simmalugnt.se>
2025-04-16 11:07:50 +0200

    removed https in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+    //   cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+    // },
   },
   build: {
     outDir: "dist",
